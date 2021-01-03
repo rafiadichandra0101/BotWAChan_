c37c2503f55fb5010662d6c825b137b9df476a5d
rafiadichandra0101 <75437271+rafiadichandra0101@users.noreply.github.com>
2021-01-03 10:51:55 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,61 @@
+const help = (prefix, pushname) => {
+	return `✪═⟪ CALIPH BOT MENU ⟫═✪
+
+*AUTHOR : CALIPH A*
+*BOT TYPE : TERMUX*
+
+╔════════════════════
+║ Hai Kak *${pushname}*
+╠════════════════════
+║╭──❉ *INFO* ❉──
+║│1. ${prefix}info
+║│2. ${prefix}bugreport <lapor bug>
+║│3. ${prefix}runtime
+║│4. ${prefix}join <linkgroup>
+║╰───────────
+║╭──❉ *GROUP MENU* ❉──
+║│1. ${prefix}clone
+║│2. ${prefix}promote <@tagmember>
+║│3. ${prefix}demote <@tagadmin>
+║│4. ${prefix}tagall <1 atau 2 atau 3>
+║│5. ${prefix}simih <0 atau 1>
+║│6. ${prefix}group <open atau close>
+║│8. ${prefix}setdesc <teks>
+║│9. ${prefix}setpp 
+║│10. ${prefix}setname <teks>
+║│11. ${prefix}kick <@tagmember>
+║│12. ${prefix}linkgroup
+║╰───────────
+║╭──❉ *MEDIA* ❉──
+║│1. ${prefix}toimg <reply stiker>
+║│2. ${prefix}sticker
+║│3. ${prefix}ttp <teks>
+║│4. ${prefix}sticker nobg <ERROR>
+║│5. ${prefix}tts <kode bahasa> <teks>
+║│6. ${prefix}url2img <tipe> <url>
+║│7. ${prefix}wait <kirim atau reply foto>
+║│8. ${prefix}ocr
+║│9. ${prefix}nulis <teks>
+║╰───────────
+║╭──❉ *OWNER MENU* ❉──
+║│1. ${prefix}setprefix <prefix>
+║│2. ${prefix}bc <promosi>
+║│3. ${prefix}setppbot 
+║│4. ${prefix}clone @tagmember
+║╰───────────
+║╭──❉ *IKLAN* ❉──
+║│1. Instagram
+║│ @caliph91_
+║│2. Creator Caliph Bot
+║│ https://wa.me/6281215199447
+║╰───────────
+║╭──❉ *PERATURAN* ❉──
+║│1. JANGAN NELPON
+║│2. JANGAN SPAM
+║╰───────────
+╠════════════════════
+║  _*END*_
+╚════════════════════`
+}
+
+exports.help = help
